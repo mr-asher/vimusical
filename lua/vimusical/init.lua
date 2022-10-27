c336eca2726db43d5414bdde841e963985ed7c55
Asher <jasherleslie@gmail.com>
2022-10-27 15:20:21 +1100

    Back to auto play.

--- lua/vimusical/init.lua
+++ lua/vimusical/init.lua
@@ -2,8 +2,4 @@ local function playHaxorMusic()
   io.popen("mpv --no-video \"https://www.youtube.com/watch?v=9D-QD_HIfjA\"")
 end
 
-local M = {}
-
-M.playHaxorMusic = playHaxorMusic
-
-return M
+playHaxorMusic()
